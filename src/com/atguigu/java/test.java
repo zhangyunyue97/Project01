91386c3005f271b72f4f7feafc033d71e222975b
zhangyunyue97 <zhangyunyue97@163.com>
2018-11-21 23:30:37 +0800

    Initial commit

---
+++ src/com/atguigu/java/test.java
@@ -0,0 +1,25 @@
+package com.atguigu.java;
+/**
+ * @Description:    java类作用描述
+ * @Author:         ZYY
+ * @CreateDate:     2018/11/21 16:03
+*/
+public class test {
+    public static final int NUM = 10;
+    /**
+     * id
+     */
+    private int id = 30;
+
+    /**
+     * 方法实现说明
+     * @author      ZYY
+     * @param
+     * @return      
+     * @exception   
+     * @date        2018/11/21 16:09
+    */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/atguigu/java/test2.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author ZYY
+ * @data 2018/11/20 - 21:31
+ */
+public class test2 {
+}
